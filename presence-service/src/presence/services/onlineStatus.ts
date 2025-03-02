1fa0c61bff1fcdab1fd12517bb3ade0868d7a4e9
Azzam Hanif <azzam.hanif@privyid.tech>
2025-03-02 15:33:32 +0700

    feat: enhance presence service with error handling and response formatting for last seen, online, and typing status

--- presence-service/src/presence/services/onlineStatus.ts
+++ presence-service/src/presence/services/onlineStatus.ts
@@ -1,18 +1,29 @@
 import * as redis from '../../shared/redisAdapter';
 import { IPubSubMQ, PubsubRedisMQ } from '../pubsubAdapter';
 import * as dao from '../dao';
+import { InternalServerErrorResponse, OkResponse } from "@src/shared/commons/patterns";
 
 const pubSub: IPubSubMQ = new PubsubRedisMQ();
 
-export async function getOnlineStatus(username: string): Promise<boolean> {
-  let isOnline =  await redis.getKey(`status:online:username:${username}`)
-  return isOnline === 'true'
+export async function getOnlineStatus(username: string) {
+  try{
+    let isOnline =  await redis.getKey(`status:online:username:${username}`)
+    return new OkResponse({ is_online: isOnline === 'true' }).generate()
+  } catch (err: any) {
+    return new InternalServerErrorResponse(err).generate()
+  }
 }
 
-export async function setOnlineStatus(userId: string, username: string, status: boolean): Promise<void> {
-  const last_seen = new Date();
-  await redis.setKey(`status:online:username:${username}`, `${status}`)
-  await redis.setKey(`status:last_seen:username:${username}`, last_seen.toISOString())
-  dao.upsertUserLastSeen(userId, last_seen);
-  await pubSub.publishOnlineStatus(userId, status)
+export async function setOnlineStatus(userId: string, username: string, status: boolean) {
+  try{
+    const last_seen = new Date();
+    await redis.setKey(`status:online:username:${username}`, `${status}`)
+    await redis.setKey(`status:last_seen:username:${username}`, last_seen.toISOString())
+    dao.upsertUserLastSeen(userId, last_seen);
+    await pubSub.publishOnlineStatus(userId, status)
+    return new OkResponse({status: 'success'}).generate()
+  } catch (err: any) {
+    // console.error('setOnlineStatus error:', err);
+    return new InternalServerErrorResponse(err).generate()
+  }
 }
--- presence-service/src/presence/services/typingStatus.ts
+++ presence-service/src/presence/services/typingStatus.ts
@@ -1,14 +1,24 @@
 import * as redis from '../../shared/redisAdapter';
 import { IPubSubMQ, PubsubRedisMQ } from '../pubsubAdapter';
+import { InternalServerErrorResponse, OkResponse } from "@src/shared/commons/patterns";
 
 const pubSub: IPubSubMQ = new PubsubRedisMQ();
 
-export async function getTypingStatus(username: string): Promise<boolean> {
-  let isTyping =  await redis.getKey(`status:typing:username:${username}`)
-  return isTyping === 'true'
+export async function getTypingStatus(username: string) {
+  try{
+    let isTyping =  await redis.getKey(`status:typing:username:${username}`)
+    return new OkResponse({is_typing: isTyping === 'true'}).generate()
+  } catch (err: any) {
+    return new InternalServerErrorResponse(err).generate()
+  }
 }
 
-export async function setTypingStatus(userId: string, username: string, status: boolean): Promise<void> {
-  await redis.setKey(`status:typing:username:${username}`, `${status}`)
-  await pubSub.publishTypingStatus(username, status)
+export async function setTypingStatus(userId: string, username: string, status: boolean) {
+  try{
+    await redis.setKey(`status:typing:username:${username}`, `${status}`)
+    await pubSub.publishTypingStatus(username, status)
+    return new OkResponse({status: 'success'}).generate()
+  } catch (err: any) {
+    return new InternalServerErrorResponse(err).generate()
+  }
 }
